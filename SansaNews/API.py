a96027b951bb1d732651551611e579cd0a3a1b00
MoonTurtlee <joaquin.dominguez@usm.cl>
2022-10-29 13:27:01 -0300

    Ahora se ve una imageeeeeen!!11!!!111!uno 	modified:   Proyecto/__pycache__/settings.cpython-310.pyc 	modified:   Proyecto/settings.py 	modified:   SansaNews/API.py 	new file:   SansaNews/__pycache__/API.cpython-310.pyc 	modified:   SansaNews/__pycache__/views.cpython-310.pyc 	deleted:    SansaNews/gbu_usm/2022-06-02_17-26-23_UTC.json 	deleted:    SansaNews/gbu_usm/2022-06-23_01-34-54_UTC.json 	deleted:    SansaNews/gbu_usm/2022-06-27_15-13-11_UTC.json 	deleted:    SansaNews/gbu_usm/2022-07-09_01-37-28_UTC.json 	deleted:    SansaNews/json_reader.py 	modified:   SansaNews/templates/GBU.html 	modified:   SansaNews/views.py 	deleted:    gbu_usm/2022-04-19_04-58-33_UTC.json 	deleted:    gbu_usm/2022-04-19_04-59-02_UTC.json 	deleted:    gbu_usm/2022-04-19_04-59-24_UTC.json 	deleted:    gbu_usm/2022-08-24_03-31-08_UTC.json 	deleted:    gbu_usm/2022-09-29_12-41-51_UTC.json 	deleted:    gbu_usm/gbu_usm_52691001886.json 	deleted:    gbu_usm/id 	new file:   static/gbu_usm/2022-04-19_04-58-33_UTC.jpg 	renamed:    SansaNews/gbu_usm/2022-04-19_04-58-33_UTC.json -> static/gbu_usm/2022-04-19_04-58-33_UTC.json 	new file:   static/gbu_usm/2022-04-19_04-59-02_UTC.jpg 	renamed:    SansaNews/gbu_usm/2022-04-19_04-59-02_UTC.json -> static/gbu_usm/2022-04-19_04-59-02_UTC.json 	new file:   static/gbu_usm/2022-04-19_04-59-24_UTC.jpg 	renamed:    SansaNews/gbu_usm/2022-04-19_04-59-24_UTC.json -> static/gbu_usm/2022-04-19_04-59-24_UTC.json 	renamed:    gbu_usm/2022-06-02_17-26-23_UTC.json -> static/gbu_usm/2022-06-02_17-26-23_UTC.json 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_1.jpg 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_2.jpg 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_3.jpg 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_4.jpg 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_5.jpg 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_6.jpg 	new file:   static/gbu_usm/2022-06-02_17-26-23_UTC_7.jpg 	renamed:    gbu_usm/2022-06-23_01-34-54_UTC.json -> static/gbu_usm/2022-06-23_01-34-54_UTC.json 	new file:   static/gbu_usm/2022-06-23_01-34-54_UTC_1.jpg 	new file:   static/gbu_usm/2022-06-23_01-34-54_UTC_2.jpg 	new file:   static/gbu_usm/2022-06-27_15-13-11_UTC.jpg 	renamed:    gbu_usm/2022-06-27_15-13-11_UTC.json -> static/gbu_usm/2022-06-27_15-13-11_UTC.json 	renamed:    gbu_usm/2022-07-09_01-37-28_UTC.json -> static/gbu_usm/2022-07-09_01-37-28_UTC.json 	new file:   static/gbu_usm/2022-07-09_01-37-28_UTC_1.jpg 	new file:   static/gbu_usm/2022-07-09_01-37-28_UTC_2.jpg 	new file:   static/gbu_usm/2022-07-09_01-37-28_UTC_3.jpg 	new file:   static/gbu_usm/2022-07-09_01-37-28_UTC_4.jpg 	new file:   static/gbu_usm/2022-08-24_03-31-08_UTC.jpg 	renamed:    SansaNews/gbu_usm/2022-08-24_03-31-08_UTC.json -> static/gbu_usm/2022-08-24_03-31-08_UTC.json 	new file:   static/gbu_usm/2022-09-29_12-41-51_UTC.jpg 	renamed:    SansaNews/gbu_usm/2022-09-29_12-41-51_UTC.json -> static/gbu_usm/2022-09-29_12-41-51_UTC.json 	renamed:    SansaNews/gbu_usm/gbu_usm_52691001886.json -> static/gbu_usm/gbu_usm_52691001886.json 	renamed:    SansaNews/gbu_usm/id -> static/gbu_usm/id

--- SansaNews/API.py
+++ SansaNews/API.py
@@ -1,5 +1,77 @@
-import instaloader
-#archivo_users = open("users.txt")
-L = instaloader.Instaloader(download_pictures=False, compress_json=False, download_videos=False,post_metadata_txt_pattern="")
-perfil = "dead.vibes.x"
-L.download_profile(perfil,fast_update=True)
+#Importar los modulos a usar y ponemos el contexto.
+import instaloader; import os; from os import path; from os.path import join,isdir ; import json
+
+def actualizar():
+    L = instaloader.Instaloader(post_metadata_txt_pattern="",compress_json=False)
+    #Descargamos los archivos necesarios de los usuarios que se encuentran en el archivo señalado
+    Archivo_Iniciativas = open("Iniciativas_tester.txt")
+    for linea in Archivo_Iniciativas:
+        profile = linea.strip()
+        L.download_profile(profile, fast_update=True, profile_pic=False)
+    Archivo_Iniciativas.close()
+    return()
+
+def contenido(pagina):
+    diccionario = {}
+    contador = 0
+    contenido = os.listdir()
+    m = ""
+    #A partir de aquí ni yo entiendo qué estoy haciendo.
+    directorio = os.path.dirname("Iniciativas_tester.txt") + "{}/"
+    lista = []
+    lista_multi_imagenes = []
+    lista_descripción = []
+    contenido_in_carpeta = sorted(os.listdir(directorio.format(pagina)))
+    formato_temp = os.path.dirname(directorio.format(pagina)) + "/{}"
+    for file in contenido_in_carpeta:
+        if m == "":
+            m = file[:24]
+            if file.endswith(".jpg"):
+                    lista_multi_imagenes.append(formato_temp.format(file))
+            if file.endswith(".json"):
+                archivo_json = open(formato_temp.format(file))
+                data = json.load(archivo_json)
+                if "edge_media_to_caption" not in data['node']:
+                    trash = 0 #LOL
+                elif len(data['node']["edge_media_to_caption"]["edges"]) == 0:
+                    lista_descripción.append("")
+                else:
+                    lista_descripción.append(data['node']["edge_media_to_caption"]["edges"][0]['node']['text'])
+        else:
+            if file.startswith(m):
+                if file.endswith(".jpg"):
+                        lista_multi_imagenes.append(formato_temp.format(file))
+                if file.endswith(".json"):
+                    archivo_json = open(formato_temp.format(file))
+                    data = json.load(archivo_json)
+                    if "edge_media_to_caption" not in data['node']:
+                        trash = 0 #LOL
+                    elif len(data['node']["edge_media_to_caption"]["edges"]) == 0:
+                        lista_descripción.append("")
+                    else:
+                        lista_descripción.append(data['node']["edge_media_to_caption"]["edges"][0]['node']['text'])
+            else:
+                m = ""
+                lista_descripción = []
+                lista_multi_imagenes = []
+                lista_publicación = []
+                lista_publicación.append(lista_multi_imagenes); lista_publicación.append(lista_descripción)
+                lista.append(lista_publicación) ; lista_publicación = []
+                #'''
+                if file.endswith(".jpg"):
+                        lista_multi_imagenes.append(formato_temp.format(file))
+                if file.endswith(".json"):
+                    archivo_json = open(formato_temp.format(file))
+                    data = json.load(archivo_json)
+                    if "edge_media_to_caption" not in data['node']:
+                        trash = 0 #LOL
+                    elif len(data['node']["edge_media_to_caption"]["edges"]) == 0:
+                        lista_descripción.append("")
+                    else:
+                        lista_descripción.append(data['node']["edge_media_to_caption"]["edges"][0]['node']['text'])
+                #Juaco: oye, yo conozco ese código...
+                #Benja: ehe.
+                #'''
+    if pagina not in diccionario:
+        diccionario[pagina] = lista
+    return diccionario
--- SansaNews/views.py
+++ SansaNews/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-from . import json_reader
+from . import API
 # Create your views here.
 
 def gbu_usm(request):
-    ruta_carpeta = "C:/Users/PlayF/Desktop/App Web/SansaNews/SansaNews/Iniciativas/" + "{}/"
-    texto = "C:/Users/PlayF/Desktop/App Web/SansaNews/SansaNews/Iniciativas/"
-    diccionario = json_reader.contenido(texto,ruta_carpeta)
-    pagina = "gbu_usm"
-    return render(request,"GBU.html",context={ "llave" : diccionario[pagina][-2]})
+    return render(request,"GBU.html")
     #context={ "llave" : [diccionario[pagina][-2],diccionario[pagina][-3],diccionario[pagina][-4],diccionario[pagina][-5],diccionario[pagina][-6]]}
