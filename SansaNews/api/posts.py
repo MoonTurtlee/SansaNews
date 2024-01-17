7385bc43cb2db9d0c42250c2f6f6ba88642a41da
LuckJMG <25126199+LuckJMG@users.noreply.github.com>
2024-01-17 12:59:31 -0300

    reorganización de nueva api

---
+++ SansaNews/api/posts.py
@@ -0,0 +1,150 @@
+import os
+from urllib import request
+from instagrapi import Client
+from . import iniciativa as api_iniciativa
+
+def descargar(iniciativa: dict, posts: list, cantidad: int, 
+                    directorio: str) -> dict:
+    '''
+    Descarga los posts más recientes de una iniciativa
+
+    Args:
+        iniciativa (dict): diccionario con la información de la iniciativa
+        posts (list): lista de posts que da instragrapi
+        cantidad (int): cantidad de posts a descargar
+        directorio (str): directorio de iniciativas
+
+    Returns:
+        dict: diccionario actualizado con la información de la iniciativa
+    '''
+    iniciativa_path: str = os.path.join(directorio, iniciativa["usuario"])
+
+    count = 0
+    for post in posts:
+        if count >= cantidad:
+            break
+
+        # Obtener datos del post
+        post_data: dict = post.model_dump()
+        datetime = str(int(post_data["taken_at"].timestamp()))
+        print(f"[API] Descargando post publicado en {datetime}...")
+
+        # Crear carpeta de post
+        post_folder: str = os.path.join(iniciativa_path, datetime)
+        try:
+            os.mkdir(post_folder)
+        except:
+            print(f"[ERROR]: No se pudo crear la carpeta del post {datetime}")
+            continue
+
+        post = {
+            "descripcion": post_data["caption_text"],
+            "media": []
+        }
+
+        # En caso de haber una sola imagen
+        if len(post_data["resources"]) == 0:
+            media_path: str = os.path.join(post_folder, f"{datetime}_0.jpg")
+            media_url = str(post_data['thumbnail_url'])
+
+            # Descargar imagen
+            print(f"[API]: Descargando media {media_url}...")
+            request.urlretrieve(media_url, media_path)
+
+        # En caso de haber multiples imagenes
+        else:
+            index = 0
+            for media in post_data["resources"]:
+                media_path: str = os.path.join(post_folder, f"{datetime}_{index}.jpg")
+                media_url = str(media['thumbnail_url'])
+
+                # Descargar imagen
+                print(f"[API]: Descargando media {media_url}...")
+                request.urlretrieve(media_url, media_path)
+
+                # Añadir media al post
+                post["media"].append(media_path)
+                index += 1
+
+        # Añadir post al diccionario de la iniciativa
+        iniciativa["posts"][str(datetime)] = post
+        count += 1
+
+    usuario: str = iniciativa["usuario"]
+    print(f"[API]: Descarga de posts de la iniciativa {usuario} finalizada")
+    return iniciativa
+
+
+def redescargar(client: Client, usuario: str, max_posts: int, 
+                directorio: str) -> dict:
+    '''
+    Fuerza la redescarga de los posts del usuario dado, borrando todos los
+    posts previamente descargados y descargandolos de nuevo
+    
+    Args:
+        client (Client): cliente de instagrapi
+        usuario (str): usuario de instagram al que redescargar los posts
+        max_posts (int): cantidad máxima de posts de la iniciativa
+        directorio (str): directorio de iniciativas
+
+    Returns:
+        dict: diccionario con la información de la iniciativa
+    '''
+    print(f"[DEBUG]: Redescargando posts de {usuario}")
+
+    iniciativa: dict = api_iniciativa.cargar(usuario, directorio)
+    limpiar(iniciativa, 99999, directorio)
+    iniciativa = api_iniciativa.actualizar(client, usuario, max_posts, 
+                                           directorio)
+
+    print(f"[DEBUG]: Posts de {usuario} redescargados con exito")
+    return iniciativa
+
+
+def limpiar(iniciativa: dict, max_posts: int, directorio: str) -> dict:
+    '''
+    Elimina los posts de una iniciativa en caso de que la iniciativa supere
+    la cantidad de posts máximos
+
+    Args:
+        iniciativa (dict): diccionario con la información de la iniciativa
+        max_posts (int): cantidad máxima de posts que puede tener una iniciativa
+            descargados a la vez
+        directorio (str): directorio de iniciativas
+
+    Returns:
+        dict: diccionario con la información de la iniciativa actualizada
+    '''
+    usuario: str = iniciativa["usuario"]
+    if len(iniciativa["posts"].keys()) <= max_posts:
+        print(f"[API]: {usuario} no excede el límite de posts")
+        return iniciativa
+
+    iniciativa_path: str = os.path.join(directorio, usuario)
+    posts: list = os.listdir(iniciativa_path)
+    posts.remove(f"{usuario}.json")
+    posts.remove(f"{usuario}.jpg")
+    posts.sort(reverse = True)
+
+    # Eliminar posts
+    print(f"[API]: Eliminando posts antiguos de {usuario}...")
+    for post in posts:
+        if len(posts) <= max_posts:
+            break
+
+        post_path: str = os.path.join(iniciativa_path, post)
+        print(f"[API]: Eliminando post {post_path}...")
+        medias: list = os.listdir(post_path)
+
+        # Eliminar archivos
+        for media in medias:
+            os.remove(os.path.join(post_path, media))
+
+        # Eliminar carpeta
+        os.rmdir(post_path)
+
+        iniciativa["posts"].pop(post)
+        posts.remove(post)
+
+    print(f"[API]: Posts de {usuario} limpiados con exito")
+    return iniciativa
--- SansaNews/views.py
+++ SansaNews/views.py
@@ -4,13 +4,15 @@ from django.http import JsonResponse #, HttpResponseRedirect
 from django.shortcuts import render, redirect
 # from django.views.decorators.csrf import csrf_exempt
 from . import API
-from .api import api, debug
 from . import forms
 from . import models
 from instagrapi import Client
+import api.iniciativa, api.posts
+from api.iniciativa import TipoIniciativa
 
 SRC = os.path.dirname(os.path.dirname(__file__))
-api.DIRECTORIO = os.path.join(SRC, "/static/iniciativas")
+DIRECTORIO = os.path.join(SRC, "/static/iniciativas")
+MAX_POSTS = 5
 
 INSTAGRAM = Client()
 INSTAGRAM.delay_range = [1, 3]
@@ -25,23 +27,22 @@ agrupaciones = {
     "redes": [],
 }
 
-
 def home(request):
-    recientes = API.recientes(4)
-    iniciativas = api.escanear_iniciativas()
+    # recientes = api..obtener_recientes(4)
+    iniciativas = api.iniciativa.escanear(DIRECTORIO)
 
     for usuario, data in iniciativas.keys():
         if data["slider"]:
             slider.append(usuario)
 
         match data["tipo"]:
-            case api.TipoIniciativa.DEPORTE:
+            case TipoIniciativa.DEPORTE:
                 agrupaciones["deportes"].append(usuario)
-            case api.TipoIniciativa.RECREACION:
+            case TipoIniciativa.RECREACION:
                 agrupaciones["recreacion"].append(usuario)
-            case api.TipoIniciativa.CENTRO:
+            case TipoIniciativa.CENTRO:
                 agrupaciones["centros"].append(usuario)
-            case api.TipoIniciativa.RED:
+            case TipoIniciativa.RED:
                 agrupaciones["redes"].append(usuario)
 
     return render(request,"Home.html",{
@@ -55,31 +56,31 @@ def home(request):
 
 
 def actualizar_iniciativas(request):
-    iniciativas: dict = api.escanear_iniciativas()
-    carpetas: list = os.listdir(api.DIRECTORIO)
+    iniciativas: dict = api.iniciativa.escanear(DIRECTORIO)
+    carpetas: list = os.listdir(DIRECTORIO)
 
     for usuario, data in iniciativas.items():
         if usuario not in carpetas:
-            api.crear_iniciativa(INSTAGRAM, usuario, data["nombre"],
-                                data["tipo"], data["slider"])
+            api.iniciativa.crear(INSTAGRAM, usuario, data["nombre"],
+                                data["tipo"], data["slider"], DIRECTORIO)
 
     return home(request)
 
 
 def limpiar_iniciativas(request):
-    iniciativas: dict = api.escanear_iniciativas()
+    iniciativas: dict = api.iniciativa.escanear(DIRECTORIO)
     usuarios: list = list(iniciativas.keys())
-    carpetas: list = os.listdir(api.DIRECTORIO)
+    carpetas: list = os.listdir(DIRECTORIO)
 
     for carpeta in carpetas:
         if carpeta not in usuarios:
-            debug.eliminar_iniciativa(carpeta)
+            api.iniciativa.eliminar(carpeta, DIRECTORIO)
 
     return home(request)
 
 
 def iniciativa(request, usuario):
-    iniciativa: dict = api.cargar_iniciativa(usuario)
+    iniciativa: dict = api.iniciativa.cargar(usuario, DIRECTORIO)
     return render(request, "Molde.html", {
         "usuario": usuario,
         "nombre": iniciativa["nombre"],
@@ -90,17 +91,17 @@ def iniciativa(request, usuario):
 
 
 def actualizar_posts(request, usuario):
-    api.actualizar_iniciativa(INSTAGRAM, usuario)
+    api.iniciativa.actualizar(INSTAGRAM, usuario, MAX_POSTS, DIRECTORIO)
     return iniciativa(request, usuario)
 
 
 def actualizar_perfil(request, usuario: str):
-    debug.actualizar_perfil(INSTAGRAM, usuario)
+    api.iniciativa.perfil(INSTAGRAM, usuario, DIRECTORIO)
     return iniciativa(request, usuario)
 
 
 def redescargar_posts(request, usuario: str):
-    debug.redescargar_posts(INSTAGRAM, usuario)
+    api.posts.redescargar(INSTAGRAM, usuario, MAX_POSTS, DIRECTORIO)
     return iniciativa(request, usuario)
 
 
@@ -124,8 +125,6 @@ def subir_avisos(request, iniciativas=None):
             return redirect(avisos)
     context = {"imagen": imagen, "iniciativas": slider}
     return render(request, 'Subir_Avisos.html', context)
-
-
 
 
 def redireccion(request):
