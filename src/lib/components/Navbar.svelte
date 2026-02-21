b93c635de18b2b935fa2dac47a5ac4622b1befca
Joaquín <dominguezbustosjoaquin@gmail.com>
2026-02-21 16:37:16 -0300

    refactor: navbar logic

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-    import { page } from '$app/state';
-    import { resolve } from '$app/paths';
+  import { page } from "$app/state";
+  import type { NavItem } from "./SuperNav.svelte";
 
-    let { navItems, logo } = $props();
+  let { navItems, logo }: { navItems: NavItem[]; logo: string } = $props();
 </script>
-
 
 <!-- Logo -->
 <div class="hidden lg:block lg:mb-6">
-  <a href={resolve("/")}>
+  <a href="/">
     <img
       src={logo}
       class="mx-auto w-full max-w-lg"
@@ -19,26 +18,21 @@
 
 <!-- Navbar -->
 <div class="hidden items-center justify-center lg:flex">
-  
   <div class="bg-primary/40 h-0.5 w-16"></div>
   <nav class="flex items-center">
     {#each navItems as item, i}
-
-      <!-- Items -->
       {@const isActive = page.url.pathname === item.href}
       <a
-          href={item.href}
-          class= "px-4 py-2 text-sm font-semibold transition-colors hover:text-primary focus:text-primary whitespace-nowrap"
-          class:text-primary={isActive}
-      > 
-          {item.label}        
+        href={item.href}
+        class="px-4 py-2 text-sm font-semibold transition-colors hover:text-primary focus:text-primary whitespace-nowrap"
+        class:text-primary={isActive}
+      >
+        {item.label}
       </a>
 
-      <!-- Separator -->
       {#if i < navItems.length - 1}
-          <span class="mx-1 select-none">/</span>
+        <span class="mx-1 select-none">/</span>
       {/if}
-
     {/each}
   </nav>
   <div class="bg-primary/40 h-0.5 w-16"></div>
